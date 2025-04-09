9a65cb097716ddf571cc0bb24d08777a9b81110f
Olar13b <olar13b@gmail.com>
2025-04-09 17:05:00 +0300

    Initial commit

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  
+  const changeColorButton = document.querySelector('.change-color');
+  const colorSpan = document.querySelector('.color');
+  const body = document.body;
+  
+  changeColorButton.addEventListener('click', () => {
+    const newColor = getRandomHexColor(); // Генеруємо новий випадковий колір
+    body.style.backgroundColor = newColor; // Задаємо фон <body>
+    colorSpan.textContent = newColor; // Вставляємо колір у span.color
+  });
